67027ad9e4eac4475aa7a1ff2d15a7a42344088e
foxwo001 <appfactory@macmini-615920.local>
2016-09-30 18:05:45 -0500

    initial project setup

---
+++ Algae Estimator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Algae Estimator
+//
+//  Created by App Factory on 9/30/16.
+//  Copyright © 2016 Software Engineering. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Algae EstimatorTests/Algae_EstimatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Algae_EstimatorTests.swift
+//  Algae EstimatorTests
+//
+//  Created by App Factory on 9/30/16.
+//  Copyright © 2016 Software Engineering. All rights reserved.
+//
+
+import XCTest
+@testable import Algae_Estimator
+
+class Algae_EstimatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
