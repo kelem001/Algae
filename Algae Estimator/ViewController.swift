7e48821edd327372e40c652c3bce00279df26d28
andrew <andrew1094@gmail.com>
2016-10-02 20:32:05 -0500

    Initial Commit

---
+++ Algae Estimator/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Algae Estimator
+//
+//  Created by App Factory on 9/30/16.
+//  Copyright © 2016 Software Engineering. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MAKE: Properties
+    @IBOutlet weak var PO4DirectBack: UIButton!
+    @IBOutlet weak var PO4DirectTopText: UILabel!
+    @IBOutlet weak var PO4DirectSlider: UISegmentedControl!
+    @IBOutlet weak var PO4DirectLabel: UILabel!
+    @IBOutlet weak var PO4DirectInputBox: UITextField!
+    @IBOutlet weak var PO4DirectHelp: UIButton!
+    @IBOutlet weak var PO4DirectSubmit: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MAKE: Actions
+    
+
+}
+
