6b52a22df26be91eb55e0b7034b1a66473b873fc
Mikial <TheUser@michaelhornings-macbook-pro.local>
2016-10-05 17:30:00 -0500

    got the four views up. the "calculate","datalog" "submit button" that goes into "data" and "graph" page. Still need a back button from data/graph page. No values are saved just UI stuff implemented

--- Algae Estimator/DataViewController.swift
+++ Algae Estimator/DataViewController.swift
@@ -1,25 +1,26 @@
 //
-//  ViewController.swift
+//  DataViewController.swift
 //  Algae Estimator
 //
-//  Created by App Factory on 9/30/16.
+//  Created by MichaelHorning on 10/5/16.
 //  Copyright © 2016 Software Engineering. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class DataViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
 
