fa01fa7e2ad2d37648d324f2dc48ddb323ec9152
foxwo001 <appfactory@macmini-615920.local>
2016-10-07 22:48:52 -0500

    CHL value grabs the box

--- Algae Estimator/CalculateViewController.swift
+++ Algae Estimator/CalculateViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class CalculateViewController: UIViewController {
 
+    //All IBOUTLETS are properties, text boxes are UITextField and
+    //UILabel is the results label
     @IBOutlet weak var tempSurface: UITextField!
     @IBOutlet weak var tempBottom: UITextField!
     @IBOutlet weak var tempDiffLabel: UILabel!
@@ -23,8 +25,17 @@ class CalculateViewController: UIViewController {
     @IBOutlet weak var lakeDepthBox: UITextField!
     @IBOutlet weak var pavLabel: UILabel!
     
+    
+    @IBOutlet weak var chlBox: UITextField!
+    @IBOutlet weak var chLabel: UILabel!
+    
+    //For now, this button calculates the results, making sure this all
+    //works. Will need to use submit button and have results project to
+    //another page
+    
     @IBAction func fakeSubmit(_ sender: AnyObject) {
         //temp difference
+        //Suface - Bottom = Difference
         let result = Double(tempSurface.text!)! - Double(tempBottom.text!)!
         tempDiffLabel.text = result.description
         
@@ -33,9 +44,13 @@ class CalculateViewController: UIViewController {
         luxLabel.text = lux.description
         
         //Pav
+        //PO4 / Depth = Pav
         let pav = Double(po4Box.text!)! / Double(lakeDepthBox.text!)!
         pavLabel.text = pav.description
         
+        //Chl
+        let chl = Double(chlBox.text!)!
+        chLabel.text = chl.description
         
     }
     
