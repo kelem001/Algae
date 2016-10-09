fbcc62c0fe099b304a68bbdc1987460edf29d35c
foxwo001 <appfactory@macmini-615920.local>
2016-10-09 15:25:14 -0500

    remove testing features

--- Algae Estimator/CalculateViewController.swift
+++ Algae Estimator/CalculateViewController.swift
@@ -30,31 +30,6 @@ class CalculateViewController: UIViewController {
     @IBOutlet weak var chlBox: UITextField!
     @IBOutlet weak var chLabel: UILabel!
     
-    //For now, this button calculates the results, making sure this all
-    //works. Will need to use submit button and have results project to
-    //another page
-    
-    @IBAction func fakeSubmit(_ sender: AnyObject) {
-        //temp difference
-        //Suface - Bottom = Difference
-        let result = Double(tempSurface.text!)! - Double(tempBottom.text!)!
-        tempDiffLabel.text = result.description
-        
-        //Lux
-        let lux = Double(brightBox.text!)!
-        luxLabel.text = lux.description
-        
-        //Pav
-        //PO4 / Depth = Pav
-        let pav = Double(po4Box.text!)! / Double(lakeDepthBox.text!)!
-        pavLabel.text = pav.description
-        
-        //Chl
-        let chl = Double(chlBox.text!)!
-        chLabel.text = chl.description
-        
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
