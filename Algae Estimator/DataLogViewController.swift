db4b88b782649ad61dc775ea0a28060ff789c0b8
Dominique Tipton <dtipton@live.com>
2016-10-21 18:40:56 -0500

    Added Catalog view controller

---
+++ Algae Estimator/DataLogViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DataLogViewController.swift
+//  Algae Estimator
+//
+//  Created by Dominique Tipton on 10/21/16.
+//  Copyright © 2016 Software Engineering. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class DataLogViewController: UIViewController {
+    
+}
