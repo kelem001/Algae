8b41941058bcecd96086b1bdc2f32cb56e6c3ce2
dom-tipton <dtipton@live.com>
2016-11-14 00:59:25 -0600

    Reverted back to using labels instead of text. All previous bugs associated with labels have been fixed :)

--- src/Algae Estimator/DataSetTableViewController.swift
+++ src/Algae Estimator/DataSetTableViewController.swift
@@ -1,8 +1,8 @@
 //
-//  RealTableViewController.swift
+//  TableViewController.swift
 //  Algae Estimator
 //
-//  Created by Dominique Tipton on 11/10/16.
+//  Created by Dominique Tipton on 11/13/16.
 //  Copyright © 2016 Software Engineering. All rights reserved.
 //
 
@@ -15,26 +15,20 @@ class DataSetTableViewController: UITableViewController {
     
     let headers = ["Total Chl a", "Cyano Chl a"]
     
+    let width = Int(UIScreen.main.bounds.width)
+    let height: Int = 44 // Default cell height
+    
     override func viewWillAppear(_ animated: Bool) {
         tableView.backgroundColor = UIColor(red:0.28, green:0.31, blue:0.58, alpha:1.0)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         return headers.count
@@ -47,42 +41,28 @@ class DataSetTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! DataSetTableViewCell!
+
+        cell?.indexLabel.text = "[" + String(describing: indexPath.row * 24) + "]"
+        cell?.indexLabel.frame = CGRect(x: 0, y: 0, width: width / 2, height: height)
+        cell?.indexLabel.textAlignment = NSTextAlignment.center
+        cell?.indexLabel.textColor = UIColor.white
         
-        let text = "[" + String(describing: indexPath.row * 24) + "]\t\t\t:\t\t\t" + String(describing: data[indexPath.section][indexPath.row * 24])
+        cell?.colonLabel.text = ":"
+        cell?.colonLabel.frame = CGRect(x: 0, y: 0, width: width, height: height)
+        cell?.colonLabel.textAlignment = NSTextAlignment.center
+        cell?.colonLabel.textColor = UIColor.white
         
-        cell.textLabel?.text = text
-        cell.textLabel?.textColor = UIColor.white
-        cell.textLabel?.textAlignment = NSTextAlignment.center
+        cell?.dataLabel.text = String(describing: data[indexPath.section][indexPath.row * 24])
+        cell?.dataLabel.frame = CGRect(x: width / 2, y: 0, width: width / 2, height: height)
+        cell?.dataLabel.textAlignment = NSTextAlignment.center
+        cell?.dataLabel.textColor = UIColor.white
         
-//        let width = Int(cell.frame.width)
-//        let height = Int(cell.frame.height)
-//        
-//        let lLabel = UILabel(frame: CGRect(x: 0, y: 0, width: width / 2, height: height))
-//        lLabel.text = "[" + String(describing: indexPath.row * 24) + "]"
-//        lLabel.textAlignment = NSTextAlignment.center
-//        lLabel.textColor = UIColor.white
-//        
-//        let cLabel = UILabel(frame: CGRect(x: 0, y: 0, width: width, height: height))
-//        cLabel.text = ":"
-//        cLabel.textAlignment = NSTextAlignment.center
-//        cLabel.textColor = UIColor.white
-//
-//        NSLog("Section = \(indexPath.section) Row = \(indexPath.row) Data = \(data[indexPath.section][indexPath.row * 24])")
-//        
-//        let rLabel = UILabel(frame: CGRect(x: width / 2, y: 0, width: width / 2, height: height))
-//        rLabel.text = String(describing: data[indexPath.section][indexPath.row * 24])
-//        rLabel.textAlignment = NSTextAlignment.center
-//        rLabel.textColor = UIColor.white
-//
-//        cell.addSubview(lLabel)
-//        cell.addSubview(cLabel)
-//        cell.addSubview(rLabel)
-        
-        return cell
+        return cell!
     }
     
     override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        
         let header = view as! UITableViewHeaderFooterView
         
         header.tintColor = UIColor(red:0.28, green:0.31, blue:0.58, alpha:1.0)
@@ -93,5 +73,4 @@ class DataSetTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return headers[section]
     }
-    
 }
