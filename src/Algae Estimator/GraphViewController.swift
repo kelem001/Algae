edbc97377a7a7b24d2ae41d23744d79fc42e8e20
foxwo001 <appfactory@macmini-615920.local>
2016-11-14 20:34:22 -0600

    Graph has border and lines stay within. Format is correct, needs gridlines and correct values -Dominique

--- src/Algae Estimator/GraphViewController.swift
+++ src/Algae Estimator/GraphViewController.swift
@@ -26,17 +26,22 @@ class GraphViewController: UIViewController {
         super.viewDidLoad()
         
         //sets background color to white
-        self.view.backgroundColor = UIColor.white
+//        self.view.backgroundColor = UIColor.white
         
         let chartConfig = ChartConfigXY(
-            xAxisConfig: ChartAxisConfig(from: 2, to: 14, by: 2),
-            yAxisConfig: ChartAxisConfig(from: 0, to: 14, by: 2),
+            xAxisConfig: ChartAxisConfig(from: 0, to: 396, by: 66),
+            yAxisConfig: ChartAxisConfig(from: 0, to: 60, by: 10),
             xAxisLabelSettings: ChartLabelSettings()
         )
         
+        //ORigin = topleft
+        //x = left and right
+        //y = up and down
+        
         let chart = LineChart(
-            
-            frame: CGRect(x: 0,y: 70,width: 300,height: 500),
+//            frame: CGRect(x: 50 ,y: (self.navigationController?.navigationBar.frame.height)! + 50,width: self.view.frame.width - 100 ,height: self.view.frame.height - (self.navigationController?.navigationBar.frame.height)! - (self.tabBarController?.tabBar.frame.height)! - 50),
+//            frame: self.view.bounds,
+            frame: CGRect(x: self.view.bounds.minX + 50, y: self.view.bounds.minY + 100, width: self.view.bounds.width - 100, height: self.view.bounds.height - 200),
             chartConfig: chartConfig,
             xTitle: "X axis",
             yTitle: "Y axis",
@@ -45,6 +50,7 @@ class GraphViewController: UIViewController {
                 (chartPoints: [(2.0, 2.6), (4.2, 4.1), (7.3, 1.0), (8.1, 11.5), (14.0, 3.0)], color: UIColor.blue)
             ]
         )
+        chart.view.backgroundColor = UIColor.black
         
         self.view.addSubview(chart.view)
 
