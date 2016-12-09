a318820e924cbb8a05c888f5a97a84f20c98b125
foxwo001 <appfactory@macmini-615920.local>
2016-12-08 18:49:20 -0600

    More UI testing was done -Waleed

--- src/Algae EstimatorUITests/Algae_EstimatorUITests.swift
+++ src/Algae EstimatorUITests/Algae_EstimatorUITests.swift
@@ -80,6 +80,59 @@ class Algae_EstimatorUITests: XCTestCase {
         
     }
     
+    func testCreate2() {
+        
+        
+        let app = XCUIApplication()
+        let scrollViewsQuery = app.scrollViews
+        
+        let cTextField = app.scrollViews.children(matching: .textField).matching(identifier: "°C").element(boundBy: 0)
+        cTextField.tap()
+        
+        app.keys["2"].tap()
+        app.keys["7"].tap()
+        let nextButton = XCUIApplication().toolbars.buttons["Next"]
+        nextButton.tap()
+        
+        app.keys["4"].tap()
+        nextButton.tap()
+        
+        app.keys["2"].tap()
+        nextButton.tap()
+        
+        app.keys["9"].tap()
+        app.keys["4"].tap()
+        app.keys["8"].tap()
+        app.keys["0"].tap(
+        app.keys["7"].tap())
+        nextButton.tap()
+        
+        scrollViewsQuery.children(matching: .button).matching(identifier: "SET").element(boundBy: 0).tap()
+        
+        let po4TextField = app.scrollViews.children(matching: .textField).matching(identifier: "mg/L").element(boundBy: 0)
+        po4TextField.tap()
+        
+        app.keys["7"].tap()
+        nextButton.tap()
+        
+        app.buttons["SUBMIT"].tap()
+        
+        scrollViewsQuery.children(matching: .button).matching(identifier: "SET").element(boundBy: 0).tap()
+        
+        let totalChlTextField = app.scrollViews.children(matching: .textField).matching(identifier: "μg/L").element(boundBy: 0)
+        totalChlTextField.tap()
+        app.keys["4"].tap()
+        app.keys["1"].tap()
+        nextButton.tap()
+        app.keys["8"].tap()
+        app.keys["2"].tap()
+        
+        app.buttons["SUBMIT"].tap()
+        
+        app.buttons["SUBMIT"].tap()
+        
+    }
+    
     func testEdit() {
         
         let app = XCUIApplication()
@@ -106,8 +159,41 @@ class Algae_EstimatorUITests: XCTestCase {
         tabBarsQuery.buttons["GRAPH"].tap()
         tabBarsQuery.buttons["DATA"].tap()
         app.buttons["Data Set"].tap()
+    }
+    
+    func testQuestion() {
         
+        let app = XCUIApplication()
+        let scrollViewsQuery = app.scrollViews
+        scrollViewsQuery.children(matching: .button).matching(identifier: "?").element(boundBy: 0).tap()
         
+        let uiviewNavigationBar = app.navigationBars["UIView"]
+        let backButton = uiviewNavigationBar.children(matching: .button).matching(identifier: "Back").element(boundBy: 0)
+        backButton.tap()
+        scrollViewsQuery.children(matching: .button).matching(identifier: "?").element(boundBy: 1).tap()
+        backButton.tap()
+        scrollViewsQuery.children(matching: .button).matching(identifier: "?").element(boundBy: 2).tap()
+        backButton.tap()
+        scrollViewsQuery.children(matching: .button).matching(identifier: "?").element(boundBy: 3).tap()
+        backButton.tap()
+        scrollViewsQuery.children(matching: .button).matching(identifier: "?").element(boundBy: 4).tap()
+        backButton.tap()
+        
+        let button = scrollViewsQuery.children(matching: .button).matching(identifier: "?").element(boundBy: 5)
+        button.tap()
+        backButton.tap()
+        scrollViewsQuery.children(matching: .button).matching(identifier: "SET").element(boundBy: 0).tap()
+        
+        let button2 = app.buttons["?"]
+        button2.tap()
+        uiviewNavigationBar.buttons["PO4 Entry"].tap()
+        app.navigationBars["PO4 Entry"].children(matching: .button).matching(identifier: "Back").element(boundBy: 0).tap()
+        button.tap()
+        backButton.tap()
+        scrollViewsQuery.children(matching: .button).matching(identifier: "SET").element(boundBy: 1).tap()
+        button2.tap()
+        uiviewNavigationBar.buttons["Chl Entry"].tap()
+        app.navigationBars["Chl Entry"].children(matching: .button).matching(identifier: "Back").element(boundBy: 0).tap()
         
     }
     
