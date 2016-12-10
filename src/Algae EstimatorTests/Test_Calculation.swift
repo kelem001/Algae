18ca4af48a284267a9481b8bcb1db8fc1aed410e
qiaoLyrics <apple511324921@gmail.com>
2016-12-09 22:25:10 -0600

    Calculation test

--- src/Algae EstimatorTests/Test_Calculation.swift
+++ src/Algae EstimatorTests/Test_Calculation.swift
@@ -5,8 +5,9 @@
 //  Created by Derek Riley on 12/5/16.
 //  Copyright © 2016 Software Engineering. All rights reserved.
 //
-
+import UIKit
 import XCTest
+@testable import Algae_Estimator
 
 class Test_Calculation: XCTestCase {
     
@@ -20,16 +21,23 @@ class Test_Calculation: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testCalculationDirect(){
+        let calculations = Calculations(total_chla: Float(5.0), cyano_chla: Float(5.0), Pbot: Float(2.0), surtemp: Float(38.0), bottemp: Float(20.0), depth: Float(3.0), lux: Float(6.0))
+        
+        let K1 = calculations.getK1()
+        let K2 = calculations.getK2()
+        XCTAssert(K1 == 250, "K1 correct")
+        XCTAssert(K2 == 200, "K2 correct")
+    
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testCalculationEstimate(){
+        let calculations = Calculations(SD_value: Float(0.5), DO_value: Float(42.0), Pbot: Float(2.0), surtemp: Float(33.0), bottemp: Float(15.0), depth: Float(4.0), lux: Float(7.0), estimate: Bool(true))
+        let Total_chla = calculations.total_chla
+        let Cyano_chla = calculations.cyano_chla
+        XCTAssert(Total_chla > 37 && Total_chla < 38, "Total_chla estimate correct")
+        XCTAssert(Cyano_chla > 11 && Total_chla < 12, "Cyano_chla estimate correct")
+        
     }
     
 }
